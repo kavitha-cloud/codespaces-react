7c0c4c80a00ca23ffaa918d6f4fdbd47d3bb9172
kavitha mohan <52957239+kavitha-cloud@users.noreply.github.com>
2023-03-08 12:18:18 +0000

    square

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import square from './square';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <square/>
+    
+    <square />
+    <App />
   </React.StrictMode>
 );
 
